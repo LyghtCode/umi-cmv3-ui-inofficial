96ad3be16ea22e65e3ac3271f25a20f90fc5b587
Lyght Code <taino@olmec.ai>
2024-01-17 14:51:34 -0600

    Update settings.tsx

--- settings.tsx
+++ settings.tsx
@@ -3,7 +3,7 @@
 export const mintText = [
   { label: "WL", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
   { label: "OG", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "OG Mint" },
-  { label: "publi", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
+  { label: "public", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
   { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
 ];
 
@@ -11,4 +11,4 @@ export const mintText = [
 export const image = "https://avatars.githubusercontent.com/u/93528482?v=4";
 
 //website title
-export const headerText = "MarkSackerberg's mint UI";
+export const headerText = "MyGeoTokensMint";
