84e1d232d1200147b4a2560479a4a78222e8f446
shadowfaz16 <erikfazju@gmail.com>
2024-02-01 20:57:24 -0600

    numberz

--- components/number.tsx
+++ components/number.tsx
@@ -4,30 +4,16 @@ import {
   Umi,
 } from "@metaplex-foundation/umi";
 import { DigitalAssetWithToken, JsonMetadata } from "@metaplex-foundation/mpl-token-metadata";
-import dynamic from "next/dynamic";
 import { Dispatch, SetStateAction, useEffect, useMemo, useState } from "react";
 import { useUmi } from "../utils/useUmi";
 import { fetchCandyMachine, safeFetchCandyGuard, CandyGuard, CandyMachine, AccountVersion } from "@metaplex-foundation/mpl-candy-machine"
-import styles from "../styles/Home.module.css";
 import { guardChecker } from "../utils/checkAllowed";
-import { Center, Card, CardHeader, CardBody, StackDivider, Heading, Stack, useToast, Text, Skeleton, useDisclosure, Button, Modal, ModalBody, ModalCloseButton, ModalContent, Image, ModalHeader, ModalOverlay, Box, Divider, VStack, Flex } from '@chakra-ui/react';
-import { ButtonList } from "./mintButton";
 import { GuardReturn } from "../utils/checkerHelper";
-import { ShowNft } from "./showNft";
-import { InitializeModal } from "./initializeModal";
-import { image, headerText } from "../settings";
 import { useSolanaTime } from "@/utils/SolanaTimeContext";
-
-const WalletMultiButtonDynamic = dynamic(
-  async () =>
-    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
-  { ssr: false }
-);
 
 const useCandyMachine = (umi: Umi, candyMachineId: string, checkEligibility: boolean, setCheckEligibility: Dispatch<SetStateAction<boolean>>) => {
   const [candyMachine, setCandyMachine] = useState<CandyMachine>();
   const [candyGuard, setCandyGuard] = useState<CandyGuard>();
-  const toast = useToast();
 
 
   useEffect(() => {
@@ -35,16 +21,7 @@ const useCandyMachine = (umi: Umi, candyMachineId: string, checkEligibility: boo
       if (checkEligibility) {
         if (!candyMachineId) {
           console.error("No candy machine in .env!");
-          if (!toast.isActive("no-cm")) {
-            toast({
-              id: "no-cm",
-              title: "No candy machine in .env!",
-              description: "Add your candy machine address to the .env file!",
-              status: "error",
-              duration: 999999,
-              isClosable: true,
-            });
-          }
+
           return;
         }
 
@@ -53,26 +30,12 @@ const useCandyMachine = (umi: Umi, candyMachineId: string, checkEligibility: boo
           candyMachine = await fetchCandyMachine(umi, publicKey(candyMachineId));
           //verify CM Version
           if (candyMachine.version != AccountVersion.V2) {
-            toast({
-              id: "wrong-account-version",
-              title: "Wrong candy machine account version!",
-              description: "Please use latest sugar to create your candy machine. Need Account Version 2!",
-              status: "error",
-              duration: 999999,
-              isClosable: true,
-            });
+
             return;
           }
         } catch (e) {
           console.error(e);
-          toast({
-            id: "no-cm-found",
-            title: "The CM from .env is invalid",
-            description: "Are you using the correct environment?",
-            status: "error",
-            duration: 999999,
-            isClosable: true,
-          });
+
         }
         setCandyMachine(candyMachine);
         if (!candyMachine) {
@@ -83,14 +46,7 @@ const useCandyMachine = (umi: Umi, candyMachineId: string, checkEligibility: boo
           candyGuard = await safeFetchCandyGuard(umi, candyMachine.mintAuthority);
         } catch (e) {
           console.error(e);
-          toast({
-            id: "no-guard-found",
-            title: "No Candy Guard found!",
-            description: "Do you have one assigned?",
-            status: "error",
-            duration: 999999,
-            isClosable: true,
-          });
+
         }
         if (!candyGuard) {
           return;
@@ -114,10 +70,6 @@ const useCandyMachine = (umi: Umi, candyMachineId: string, checkEligibility: boo
 export default function Numbers() {
   const umi = useUmi();
   const solanaTime = useSolanaTime();
-  const toast = useToast();
-  const { isOpen: isShowNftOpen, onOpen: onShowNftOpen, onClose: onShowNftClose } = useDisclosure();
-  const { isOpen: isInitializerOpen, onOpen: onInitializerOpen, onClose: onInitializerClose } = useDisclosure();
-  const [mintsCreated, setMintsCreated] = useState<{ mint: PublicKey, offChainMetadata: JsonMetadata | undefined }[] | undefined>();
   const [isAllowed, setIsAllowed] = useState<boolean>(false);
   const [loading, setLoading] = useState(true);
   const [ownedTokens, setOwnedTokens] = useState<DigitalAssetWithToken[]>();
@@ -129,30 +81,12 @@ export default function Numbers() {
 
   if (!process.env.NEXT_PUBLIC_CANDY_MACHINE_ID) {
     console.error("No candy machine in .env!")
-    if (!toast.isActive('no-cm')) {
-      toast({
-        id: 'no-cm',
-        title: 'No candy machine in .env!',
-        description: "Add your candy machine address to the .env file!",
-        status: 'error',
-        duration: 999999,
-        isClosable: true,
-      })
-    }
+
   }
   const candyMachineId: PublicKey = useMemo(() => {
     if (process.env.NEXT_PUBLIC_CANDY_MACHINE_ID) {
       return publicKey(process.env.NEXT_PUBLIC_CANDY_MACHINE_ID);
     } else {
-      console.error(`NO CANDY MACHINE IN .env FILE DEFINED!`);
-      toast({
-        id: 'no-cm',
-        title: 'No candy machine in .env!',
-        description: "Add your candy machine address to the .env file!",
-        status: 'error',
-        duration: 999999,
-        isClosable: true,
-      })
       return publicKey("11111111111111111111111111111111");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -189,9 +123,6 @@ export default function Numbers() {
     // On purpose: not check for candyMachine, candyGuard, solanaTime
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [umi, checkEligibility]);
-  console.log('candymachine authority', candyMachine?.authority)
-  console.log('umi identity', umi.identity.publicKey)
-  console.log('guard', candyGuard?.groups)
 
   const PageContent = () => {
     return (
